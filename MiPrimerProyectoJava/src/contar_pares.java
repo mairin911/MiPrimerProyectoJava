97f841ba91412e80ec7d29bc79bba9d7eb4f8e0f
karol3012 <kjarevalope@ufpso.edu.co>
2025-03-07 15:41:19 -0500

    Ejercicios Prácticos de Estructuras de Control

---
+++ MiPrimerProyectoJava/src/contar_pares.java
@@ -0,0 +1,11 @@
+public class contar_pares {
+    public static void main(String[] args) {
+        int contadorPares = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                contadorPares++;
+            }
+        }
+        System.out.println("La cantidad de números pares entre 1 y 100 es: " + contadorPares);
+    }
+}
