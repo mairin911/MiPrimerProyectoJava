8060eea65fa6dc4cfe36f14077c0b125c9898370
mairin911 <168012239+mairin911@users.noreply.github.com>
2025-03-20 20:23:54 -0500

    Add files via upload

---
+++ MiPrimerProyectoJava/bin/ejercicio2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+        public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     System.out.println("ingrese una nota");
+     int Nota1 = sc.nextInt();
+     System.out.println("ingrese una nota");
+     int Nota2 = sc.nextInt();
+     System.out.println("ingrese una nota");
+     int Nota3 = sc.nextInt();
+     sc.close();
+
+     double promedio = (Nota1 + Nota2 + Nota3) / 3.0;
+            System.out.println("el promedio es: " + promedio);
+
+     if (promedio>= 7){
+        System.out.println("aprobado");
+    }
+    else {
+        System.out.println("reprobado");
+ }
+    }
+    
+}
