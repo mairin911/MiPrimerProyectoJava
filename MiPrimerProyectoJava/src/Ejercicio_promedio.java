05ca153a7fcf1a1224b8967ebf3851aa421a0892
mairin <mairinbayona854@gmail.com>
2025-03-29 07:37:58 -0500

    arreglos

--- MiPrimerProyectoJava/src/Ejercicio_promedio.java
+++ MiPrimerProyectoJava/src/Ejercicio_promedio.java
@@ -6,15 +6,13 @@ public class Ejercicio_promedio {
         int[] numeros = new int[5];
         int suma = 0;
 
-        System.out.println("Por favor, ingrese 5 números:");
-
         for (int i = 0; i < numeros.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = scanner.nextInt();
             suma += numeros[i];
         }
 
-        double promedio = suma / 5.0;
+        double promedio = suma / (double) numeros.length;
         System.out.println("El promedio de los números ingresados es: " + promedio);
 
         scanner.close();
